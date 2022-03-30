24e5f2e1a78b520ee8d8d8172a59332f7ab72fa6
Rafael <37466798+likeavenus@users.noreply.github.com>
2022-03-30 23:06:04 +0300

    develop-welcome: add welcome mobile

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,3 +1,14 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+Swiper.use([Navigation, Pagination]);
+
 export default function Welcome() {
-   
+    const swiper = new Swiper('.welcome__swiper', {
+        direction: 'horizontal',
+        loop: false,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 }
